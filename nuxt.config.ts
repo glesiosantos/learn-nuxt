2ff7737a65d1b8a6165622a0e2826c04ee49807e
Glêsio Santos <glesioss@gmail.com>
2024-03-24 20:02:26 -0300

    install tailwind to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  // app: {
+  //   baseURL: process.env.BASE_URL || "http://localhost:3000"
+  // },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
